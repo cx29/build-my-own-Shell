95530b53ed05c2e651ed98101d9ee562cde89a92
twonine <1185366392qq.com>
2025-11-15 18:07:09 +0800

    support exit program

--- src/main.cs
+++ src/main.cs
@@ -2,11 +2,16 @@ class Program
 {
     static void Main()
     {
-        var commandList = new List<string>();
+        var commandList = new List<string>()
+        {
+
+        };
         while (true)
         {
             Console.Write("$ ");
             var command = Console.ReadLine();
+            if (command == "exit 0")
+                break;
             if (!commandList.Contains(command))
             {
                 Console.WriteLine($"{command}: command not found");
