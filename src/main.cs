4c6aa4208f7458a209eb8f64b421e4aec6d05fc6
twonine <1185366392qq.com>
2025-11-15 17:40:58 +0800

    support not found command

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,11 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+        var command = Console.ReadLine();
+        var commandList=new List<string>();
+        if(!commandList.Contains(command))
+        {
+            Console.WriteLine($"{command}:command not found");
+        }
     }
 }
