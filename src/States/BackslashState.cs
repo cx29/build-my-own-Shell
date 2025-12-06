c1b77e6e09486f20ac4b20c7fda559b99e31bf92
twonine <1185366392qq.com>
2025-12-06 23:05:53 +0800

    修复在双引号内遇见反斜杠时处理所有字符的问题，只处理特殊字符

--- src/States/BackslashState.cs
+++ src/States/BackslashState.cs
@@ -4,12 +4,23 @@ using src.Interface;
 namespace src.States;
 
 /// <summary>
+/// 区分上一个模式为什么，若为 DoubleQuotes 则需要判定当前字符串是否为指定的字符串
 /// 将后面跟着的字符识正常加入
 /// </summary>
 public class BackslashState : ITokenizerState
 {
+    private readonly List<char> specialChars = ['\"', '\\', '\'', '$', 'n'];
+
     public void HandleToken(Tokenizer context, char c)
     {
+        if (context.GetLastState is DoubleQuotesState doubleQuotesState)
+        {
+            if (specialChars.Contains(c))
+            {
+                context.Append('\\');
+            }
+        }
+
         context.Append(c);
         context.SetState(context.GetLastState);
     }
